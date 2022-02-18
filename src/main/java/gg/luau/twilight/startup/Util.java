e375b1e873b367bb8d8392d91defdb00323f1cf1
Sam Pritchard <sampritchard0801@gmail.com>
2022-02-18 21:52:23 +0000

    Add util class

---
+++ src/main/java/gg/luau/twilight/startup/Util.java
@@ -0,0 +1,75 @@
+package gg.luau.twilight.startup;
+
+import net.dv8tion.jda.api.entities.Member;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Util {
+    /*
+    private static final long NAPPERS_MANAGER = 777543061441478686L;
+    private static final long NAPPERS_TRYOUT = 777543061441478686L;
+    private static final long NAPPERS_PLAYER = 777543061441478686L;
+    */
+
+    /*
+    private static final long PANDAS_MANAGER = 777543126528032798L;
+    private static final long PANDAS_TRYOUT = 777543126528032798L;
+    private static final long PANDAS_PLAYER = 777543126528032798L;
+    */
+
+    /*
+    private static final long CORGIS_MANAGER = 804420437702213693L;
+    private static final long CORGIS_TRYOUT = 804420437702213693L;
+    private static final long CORGIS_PLAYER = 804420437702213693L;
+    */
+
+    /*
+    private static final long BLOSSOMS_MANAGER = 849107689685647360L;
+    private static final long BLOSSOMS_TRYOUT = 849107689685647360L;
+    private static final long BLOSSOMS_PLAYER = 849107689685647360L;
+    */
+
+    private static final long DRAGONS_MANAGER = 777543173106434098L;
+    private static final long DRAGONS_TRYOUT = 777543173106434098L;
+    private static final long DRAGONS_PLAYER = 777543173106434098L;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Util.class);
+
+    private Util() { }
+
+    public static boolean canParticipate(Member member) {
+        return isDragonsPlayer(member) || isDragonsTryout(member);
+    }
+
+    public static boolean isDragonsManager(Member member) {
+        final var managerRole = member.getJDA().getRoleById(DRAGONS_MANAGER);
+
+        if (managerRole == null) {
+            LOGGER.error("Dragons manager role not in cache.");
+            return false;
+        }
+
+        return member.getRoles().contains(managerRole);
+    }
+
+    public static boolean isDragonsTryout(Member member) {
+        final var tryoutRole = member.getJDA().getRoleById(DRAGONS_TRYOUT);
+
+        if (tryoutRole == null) {
+            LOGGER.error("Dragons tryout role not in cache.");
+            return false;
+        }
+
+        return member.getRoles().contains(tryoutRole);
+    }
+
+    public static boolean isDragonsPlayer(Member member) {
+        final var playerRole = member.getJDA().getRoleById(DRAGONS_PLAYER);
+
+        if (playerRole == null) {
+            LOGGER.error("Dragons player role not in cache.");
+            return false;
+        }
+
+        return member.getRoles().contains(playerRole);
+    }
+}
